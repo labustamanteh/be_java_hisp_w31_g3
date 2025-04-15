f5478a1e6fa80534a7a1884f777cead9aafd81fd
labustamante_meli <lauravanesa.bustamante@mercadolibre.com.co>
2025-04-15 10:31:19 -0500

    first commit

---
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g3/BeJavaHispW31G3Application.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.be_java_hisp_w31_g3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW31G3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeJavaHispW31G3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g3/BeJavaHispW31G3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.be_java_hisp_w31_g3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW31G3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
