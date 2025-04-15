fe9eccba69ae4d53fce80c68febf3e99080e9c84
labustamante_meli <lauravanesa.bustamante@mercadolibre.com.co>
2025-04-15 13:21:49 -0500

    update dependency injection
    use lombok annotations to make dependency injection through constructors

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g3/service/PostService.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g3/service/PostService.java
@@ -1,15 +1,12 @@
 package com.mercadolibre.be_java_hisp_w31_g3.service;
 
 import com.mercadolibre.be_java_hisp_w31_g3.repository.IPostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PostService implements IPostService{
     private final IPostRepository postRepository;
-
-    @Autowired
-    public PostService(IPostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
 }
