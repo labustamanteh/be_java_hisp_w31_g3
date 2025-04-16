650fc516952584c491c42d4a676811202c72ab7d
David Alexander Varon Duarte <david.varon@mercadolibre.com.co>
2025-04-16 10:23:43 -0500

    importaciones sin utilizar eliminadas

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g3/model/User.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g3/model/User.java
@@ -1,10 +1,6 @@
 package com.mercadolibre.be_java_hisp_w31_g3.model;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
