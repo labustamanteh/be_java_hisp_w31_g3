35649d363298591f37d281e84b609d45d1b352d1
labustamante_meli <lauravanesa.bustamante@mercadolibre.com.co>
2025-04-30 14:15:59 -0500

    add getPostFollowed tests
    check that the posts are from the last two weeks

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g3/model/User.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g3/model/User.java
@@ -3,6 +3,7 @@ package com.mercadolibre.be_java_hisp_w31_g3.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class User {
     private static Long generatedId = 0L;
     private Long userId;
---
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g3/util/CustomFactory.java
@@ -0,0 +1,45 @@
+package com.mercadolibre.be_java_hisp_w31_g3.util;
+
+import com.mercadolibre.be_java_hisp_w31_g3.model.Post;
+import com.mercadolibre.be_java_hisp_w31_g3.model.Product;
+import com.mercadolibre.be_java_hisp_w31_g3.model.User;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class CustomFactory {
+
+    public static Post getPostWithoutPromo(long userId, LocalDate promoDate) {
+        Product product = Product.builder()
+                .productId(1L)
+                .productName("product1")
+                .brand("brand1")
+                .color("red")
+                .type("type1")
+                .build();
+
+        return Post.builder()
+                .userId(userId)
+                .date(promoDate)
+                .product(product)
+                .categoryId(1L)
+                .price(200.0)
+                .build();
+    }
+
+    public static User getUserWithFollowedWithTwoPosts(LocalDate datePost1, LocalDate datePost2) {
+        User userInFollowed = new User();
+        userInFollowed.setUserName("user1");
+        userInFollowed.getPosts().add(getPostWithoutPromo(userInFollowed.getUserId(), datePost1));
+        userInFollowed.getPosts().add(getPostWithoutPromo(userInFollowed.getUserId(), datePost2));
+
+        User user = new User();
+        user.setUserName("user2");
+
+        user.getFollowed().add(userInFollowed);
+        userInFollowed.getFollowers().add(user);
+        return user;
+    }
+
+}
