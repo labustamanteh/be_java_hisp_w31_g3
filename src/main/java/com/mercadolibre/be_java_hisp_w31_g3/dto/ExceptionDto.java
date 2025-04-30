9b493fed6b68e42303c9f0a51c47d61a7885c17d
frankfelipro_meli <frankfelipe.rojas@mercadolibre.com.co>
2025-04-30 10:42:44 -0500

    feat: validations and code coverage

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g3/dto/ExceptionDto.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g3/dto/ExceptionDto.java
@@ -1,12 +1,24 @@
 package com.mercadolibre.be_java_hisp_w31_g3.dto;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+import java.util.List;
+import java.util.Map;
+
 @NoArgsConstructor
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionDto {
     private String message;
+    private Map<String, List<String>> errors;
+
+    public ExceptionDto(String message) {
+        this.message = message;
+    }
+
+    public ExceptionDto(Map<String, List<String>> errors) {
+        this.errors = errors;
+    }
 }
--- src/main/java/com/mercadolibre/be_java_hisp_w31_g3/dto/ProductDto.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g3/dto/ProductDto.java
@@ -2,6 +2,7 @@ package com.mercadolibre.be_java_hisp_w31_g3.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -12,12 +13,53 @@ import java.io.Serializable;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductDto implements Serializable {
+
+    @NotNull(message = "El productId es necesario")
+    @Min(value = 1, message = "El productId debe ser mayor a 0")
     @JsonProperty("product_id")
     private Long productId;
+
+    @NotNull(message = "El nombre del producto es necesario")
+    @NotBlank(message = "El campo no puede estar vacío")
+    @Size(max = 40, message = "La longitud no puede superar los 40 caracteres")
+    @Pattern(
+            regexp  = "^[\\p{L}\\p{N} ]+$",
+            message = "El campo no puede poseer caracteres especiales"
+    )
     @JsonProperty("product_name")
     private String productName;
+
+    @NotNull(message = "El tipo del producto es necesario")
+    @NotBlank(message = "El campo no puede estar vacío")
+    @Size(max = 15, message = "La longitud no puede superar los 15 caracteres")
+    @Pattern(
+            regexp  = "^[\\p{L}\\p{N} ]+$",
+            message = "El campo no puede poseer caracteres especiales"
+    )
     private String type;
+
+    @NotNull(message = "La marca del producto es necesario")
+    @NotBlank(message = "El campo no puede estar vacío")
+    @Size(max = 25, message = "La longitud no puede superar los 25 caracteres")
+    @Pattern(
+            regexp  = "^[\\p{L}\\p{N} ]+$",
+            message = "El campo no puede poseer caracteres especiales"
+    )
     private String brand;
+
+    @NotNull(message = "El color del producto es necesario")
+    @NotBlank(message = "El campo no puede estar vacío")
+    @Size(max = 15, message = "La longitud no puede superar los 15 caracteres")
+    @Pattern(
+            regexp  = "^[\\p{L}\\p{N} ]+$",
+            message = "El campo no puede poseer caracteres especiales"
+    )
     private String color;
+
+    @Size(max = 80, message = "La longitud no puede superar los 80 caracteres")
+    @Pattern(
+            regexp  = "^[\\p{L}\\p{N} ]+$",
+            message = "El campo no puede poseer caracteres especiales"
+    )
     private String notes;
 }
