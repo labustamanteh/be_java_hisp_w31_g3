56f2fc29d02fc5d3870c818364d161a2178b5149
jcanonmeli <jeym.canon@mercadolibre.com.co>
2025-05-05 14:05:44 -0500

    fix: adjust controller name integration

--- src/test/java/com/mercadolibre/be_java_hisp_w31_g3/integration/ProductControllerTest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g3/integration/ProductControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PostControllerTest {
+public class ProductControllerTest {
         @Autowired
         private MockMvc mockMvc;
         @Autowired
