b70bf50b8935264bda980fb9cf8cd5b498276e15
frankfelipro_meli <frankfelipe.rojas@mercadolibre.com.co>
2025-05-05 15:45:25 -0500

    fix: merge conflict and resolution

--- src/test/java/com/mercadolibre/be_java_hisp_w31_g3/integration/ProductControllerTest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g3/integration/ProductControllerTest.java
@@ -13,6 +13,7 @@ import com.mercadolibre.be_java_hisp_w31_g3.util.CustomFactory;
 import com.mercadolibre.be_java_hisp_w31_g3.util.PostMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,6 +24,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
@@ -355,5 +357,58 @@ public class ProductControllerTest {
                                                 result.getResolvedException()))
                                 .andExpect(jsonPath("$.message").value(expectedResultMessage));
         }
+
+    @Test
+    @DisplayName("T-0012 - US-0006: obtiene los post de los seguidos por el usuario ")
+    void getFollowedByUserId_ReturnJsonWithPost() throws Exception {
+        //Arrange
+        User user1 = CustomFactory.getUserWithFollowedListAndPosts();
+        userRepository.add(user1);
+        LocalDate threshold = LocalDate.now().minusWeeks(2);
+        List<Long> expectedIdsLast2Weeks = user1.getFollowed().get(0).getPosts()
+                .stream()
+                .filter(p -> p.getDate().isAfter(threshold))
+                .map(Post::getPostId)
+                .toList();
+        //Act & Assert
+        mockMvc.perform(get("/products/followed/{userId}/list", user1.getUserId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.user_id").value(user1.getUserId()))
+                .andExpect(jsonPath("$.posts.length()").value(expectedIdsLast2Weeks.size()));
+    }
+
+    @Test
+    @DisplayName("T-0012 - US-0006: devuelve un error, no se encontró al usuario")
+    void getFollowedByUserId_ReturnNotFoundUser() throws Exception {
+        // Arrange
+        long missingId = 999L;
+        // Act & Assert
+        mockMvc.perform(get("/products/followed/{userId}/list", missingId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message")
+                        .value("No se encontró el usuario con el id ingresado"));
+    }
+
+    @Test
+    @DisplayName("T-0012 - US-0006: devuelve una lista vacia")
+    void getFollowedByUserId_ReturnEmptyListUser() throws Exception {
+        //Arrange
+        User user1 = new User();
+        userRepository.add(user1);
+        LocalDate threshold = LocalDate.now().minusWeeks(2);
+        //Act & Assert
+        mockMvc.perform(get("/products/followed/{userId}/list", user1.getUserId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.user_id").value(user1.getUserId()))
+                .andExpect(jsonPath("$.posts").isEmpty());
 
 }
